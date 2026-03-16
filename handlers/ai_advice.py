d3a340282a9f202ba64b5a4cc794d15321a1a5ba
savagrinch1414 <savagrinch@gmail.com>
2026-03-16 21:27:56 +0300

    исправил анализ крипты

--- handlers/ai_advice.py
+++ handlers/ai_advice.py
@@ -16,7 +16,7 @@ async def btc_analyse(message: types.Message):
         try:
             crypto_service = dp['crypto_service']
             btc_price = crypto_service.get_btc_price()
-            btc_rsi = crypto_service.get_btc_rsi("BTCUSDT")
+            btc_rsi = crypto_service.get_rsi("BTCUSDT")
             if not btc_price:
                 raise ValueError("Не удалось получить цену BTC")
         except Exception as e:
@@ -144,7 +144,7 @@ async def eth_analyse(message: types.Message):
         try:
             crypto_service = dp['crypto_service']
             eth_price = crypto_service.get_eth_price()
-            eth_rsi = crypto_service.get_eth_rsi("ETHUSDT")
+            eth_rsi = crypto_service.get_rsi("ETHUSDT")
             if not eth_price:
                 raise ValueError("Не удалось получить цену ETH")
         except Exception as e:
